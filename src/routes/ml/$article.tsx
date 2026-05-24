97d5e717a70ebded4a3660910a13f28209eb72ba
Mikhail Zuev <mikhailneue@gmail.com>
2026-05-24 23:25:35 +0300

    test article display latex

---
+++ src/routes/ml/$article.tsx
@@ -0,0 +1,26 @@
+import { createFileRoute } from "@tanstack/react-router";
+import "katex/dist/katex.min.css";
+import { LaTeX } from "./LaTeX";
+
+export const Route = createFileRoute("/ml/$article")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const { article } = Route.useParams();
+
+  return (
+    <>
+      <div>
+        Я на теме {article}
+        <p>
+          Инлайн: <LaTeX expression="E = mc^2" />
+        </p>
+        <LaTeX
+          expression="\int_0^\infty e^{-x^2} dx = \frac{\sqrt{\pi}}{2}"
+          block
+        />
+      </div>
+    </>
+  );
+}
