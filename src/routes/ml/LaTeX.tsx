c7ecd7e2d14d2deee576979259bb6c42465a7c24
Mikhail Zuev <mikhailneue@gmail.com>
2026-05-24 23:26:07 +0300

    latex test

---
+++ src/routes/ml/LaTeX.tsx
@@ -0,0 +1,26 @@
+// LaTeX.tsx
+import { useEffect, useRef } from "react";
+import katex from "katex";
+import "katex/dist/katex.min.css";
+
+interface LaTeXProps {
+  expression: string;
+  block?: boolean;
+}
+
+export function LaTeX({ expression, block = false }: LaTeXProps) {
+  const ref = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      katex.render(expression, ref.current, {
+        displayMode: block,
+        throwOnError: false,
+      });
+    }
+  }, [expression, block]);
+
+  return <span ref={ref} />;
+}
+
+export default LaTeX;
