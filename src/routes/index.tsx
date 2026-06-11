96d381f88c4e86da92632e0070b7daba3875226a
Mikhail Zuev <mikhailneue@gmail.com>
2026-06-12 00:30:31 +0300

    adding unique key due to browser warning

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,22 +10,22 @@ function Home() {
   return (
     <div className="font-googlesans text-[20vw] lg:text-[10vw] content-end justify ml-10 -mt-10">
       <p>
-        {headerStr1
-          .split("")
-          .map((char) =>
-            Object.keys(mapFont).includes(char) ? (
-              <span className="font-pixel">{char}</span>
+        {headerStr1.split("").map((char) =>
+          mapFont.includes(char) ? (
+            <span key={char} className="font-pixel">
+              {char}
+            </span>
             ) : (
               char
             ),
           )}
       </p>
-      <p className="-mt-25">
-        {headerStr2
-          .split("")
-          .map((char) =>
-            Object.keys(mapFont).includes(char) ? (
-              <span className="font-pixel">{char}</span>
+      <p className="-mt-10">
+        {headerStr2.split("").map((char) =>
+          mapFont.includes(char) ? (
+            <span key={char} className="font-pixel">
+              {char}
+            </span>
             ) : (
               char
             ),
